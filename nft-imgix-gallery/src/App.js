a30bf08709b8f73f871c2d6629cef8e68c362b7b
Lawrence Neyman <67930487+lawrenceneyman@users.noreply.github.com>
2022-03-08 14:27:45 -0800

    Created app. Added imgix regular and imgix react image

---
+++ nft-imgix-gallery/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Imgix from "react-imgix";
+
+function App() {
+  return (
+    <div className="App">
+     
+     <img src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
+        alt="imgix regular"
+        />
+
+      <Imgix src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" />;
+
+    </div>
+  );
+}
+
+export default App;

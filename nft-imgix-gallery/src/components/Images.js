c6541e4cdbd6e88f2a095c866cbac3c856bc31a3
Lawrence Neyman <67930487+lawrenceneyman@users.noreply.github.com>
2022-03-08 18:41:10 -0800

    added imgix params to react Image

--- nft-imgix-gallery/src/components/Images.js
+++ nft-imgix-gallery/src/components/Images.js
@@ -22,14 +22,23 @@ export class Images extends Component{
       <Imgix 
         className="nft"
         src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
+        /* added sizes because react image was loading largest one always*/ 
+        sizes="50vw"
+      />
+            <h3>
+              This is a naked React imgix image.
+            </h3>
+<Imgix 
+        className="nft"
+        src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
+        imgixParams={{ auto: "format,compress"  }}   
         
         /* added sizes because react image was loading largest one always*/ 
         sizes="50vw"
       />
             <h3>
-              This is a React imgix image.
+              This is a React imgix image with auto= format,compress.
             </h3>
-
                 <img 
         className="nft"
         src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=w600" 

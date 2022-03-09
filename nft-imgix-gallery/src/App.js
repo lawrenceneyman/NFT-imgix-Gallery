df9b5abb8cd2b44cd47ac6ea04457d7dcd736bbc
Lawrence Neyman <67930487+lawrenceneyman@users.noreply.github.com>
2022-03-08 17:50:01 -0800

    Changed CSS to match imgix react library, and added sizes to react imgix img

--- nft-imgix-gallery/src/App.js
+++ nft-imgix-gallery/src/App.js
@@ -18,21 +18,34 @@ function App() {
       <Imgix 
         className="nft"
         src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
+        
+        /* added sizes because react image was loading largest one always*/ 
+        sizes="50vw"
       />
-
             <p>
               This is a React imgix image.
             </p>
 
       <img 
-      className="nft"
-      src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=w600" 
-      alt="OpenSea NoAPI"
+        className="nft"
+        src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=w600" 
+        alt="OpenSea NoAPI"
         />
             <p>
-              This is an OpenSea image (no API).
+              This is an OpenSea image on the profile page (no API).
             </p>
 
+      <img 
+        className="nft"
+        src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=s0" 
+        alt="OpenSea NoAPI"
+        />
+            <p>
+              This is an OpenSea image when viewing it (no API).
+            </p>
+
+
+            
     </div>
   );
 }

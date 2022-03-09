8b61d8cab0b3302c878ec365c2b9dcf049307a3d
Lawrence Neyman <67930487+lawrenceneyman@users.noreply.github.com>
2022-03-08 19:08:18 -0800

    Added text to images and replaced opensea urls with new ones, seems they change

--- nft-imgix-gallery/src/components/Images.js
+++ nft-imgix-gallery/src/components/Images.js
@@ -8,56 +8,49 @@ export class Images extends Component{
 
         return(
 
-            <div >
+        <div >
                 
-                <img 
-      className="nft"
-      src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
-      alt="imgix regular"
-    />
-          <h3>
-            This is a regular imgix image.
-          </h3>
+            <img 
+                className="nft"
+                src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
+                alt="imgix regular"
+            />
+                <p>
+                    This is a regular imgix image. (339kb | 1920 x 1038 px)
+                </p>
       
-      <Imgix 
-        className="nft"
-        src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
-        /* added sizes because react image was loading largest one always*/ 
-        sizes="50vw"
-      />
-            <h3>
-              This is a naked React imgix image.
-            </h3>
-<Imgix 
+
+        <Imgix 
         className="nft"
         src="https://lawrence-neyman-imgix.imgix.net/chihiro016.jpg" 
         imgixParams={{ auto: "format,compress"  }}   
-        
-        /* added sizes because react image was loading largest one always*/ 
+         /* added sizes because react image was loading largest one always*/ 
         sizes="50vw"
-      />
-            <h3>
-              This is a React imgix image with auto= format,compress.
-            </h3>
-                <img 
+        />
+            <p>
+              This is a React imgix image with auto= format,compress. (26.1kb | 594 x 321 px)
+            </p>
+
+        <img 
         className="nft"
         src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=w600" 
         alt="OpenSea NoAPI"
         />
-            <h3>
-              This is an OpenSea image on the profile page (no API).
-            </h3>
+            <p>
+              This is an OpenSea image on the profile page w/o OS API. (68.9kb | 600 x 324 px)
+            </p>
 
       <img 
         className="nft"
         src="https://lh3.googleusercontent.com/goHtFaFglq8HLS5HmlZIRA8gTMNHrjStDsZJ0Jcq2LX_LsPxYzRjEPFtpSfH4OAi9N-I1NX6n2qLBVTlcSRrhcC-isseAj-cIO84G9k=s0" 
         alt="OpenSea NoAPI"
         />
-            <h3>
-              This is an OpenSea image when viewing it (no API).
-            </h3>
+            <p>
+              This is an OpenSea image when viewing it fully w/o OS API. (339kb at 1920 x 1038 px)
+            </p>
 
-            </div>
+        </div>
+        
         )
     }
 
